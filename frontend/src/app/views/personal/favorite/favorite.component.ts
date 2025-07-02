2dc837faae5833c1cbd87e41fcd61db8f2d2725f
Aleksandr <101710507+meridian285@users.noreply.github.com>
2025-07-02 10:25:30 +0300

    changed: product button

--- frontend/src/app/views/personal/favorite/favorite.component.ts
+++ frontend/src/app/views/personal/favorite/favorite.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FavoriteService} from "../../../shared/services/favorite.service";
 import {FavoritesType} from "../../../../types/favorites.type";
 import {DefaultResponseType} from "../../../../types/default-response.type";
 import {environment} from "../../../../environments/environment";
+import {CartType} from "../../../../types/cart.type";
+import {CartService} from "../../../shared/services/cart.service";
+import {ProductType} from "../../../../types/product.type";
 
 @Component({
   selector: 'favorite',
@@ -11,21 +14,28 @@ import {environment} from "../../../../environments/environment";
 })
 export class FavoriteComponent implements OnInit {
 
+  @Input() countInCart: number | undefined = 0;
+  count: number = 0;
+
   products: FavoritesType[] = [];
   serverStaticPath = environment.serverStaticPath;
 
-  constructor(private favoriteService: FavoriteService) { }
+  constructor(private favoriteService: FavoriteService,
+
+              private cartService: CartService
+  ) {
+  }
 
   ngOnInit(): void {
     this.favoriteService.getFavorites()
       .subscribe((data: FavoritesType[] | DefaultResponseType) => {
-      if ((data as DefaultResponseType).error !== undefined) {
-        const error = (data as DefaultResponseType).message;
-        throw new Error(error);
-      }
+        if ((data as DefaultResponseType).error !== undefined) {
+          const error = (data as DefaultResponseType).message;
+          throw new Error(error);
+        }
 
-      this.products = data as FavoritesType[];
-    });
+        this.products = data as FavoritesType[];
+      });
   }
 
   removeFromFavorites(id: string) {
@@ -38,5 +48,28 @@ export class FavoriteComponent implements OnInit {
 
         this.products = this.products.filter(item => item.id !== id)
       })
+  }
+
+  addToCart(productId: string) {
+    this.cartService.updateCart(productId, this.count)
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+        this.countInCart = this.count;
+      });
+  }
+
+  removeFromCart(productId: string) {
+    this.cartService.updateCart(productId, 0)
+      .subscribe((data: CartType | DefaultResponseType) => {
+        if ((data as DefaultResponseType).error !== undefined) {
+          throw new Error((data as DefaultResponseType).message);
+        }
+
+        this.countInCart = 0;
+        this.count = 1;
+      });
   }
 }
